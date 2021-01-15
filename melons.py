ff17628b5c84c218992e232fd7efb7e1edad5931
Yvonne Yeh <yvonneyeh@me.com>
2021-01-15 02:16:03 +0000

    Add Squash class and paint method

--- melons.py
+++ melons.py
@@ -31,6 +31,12 @@ class Melon(object):
                                              self.weight,
                                              self.melon_type)
 
-# FIXME: Add Squash class definition here.
+
 class Squash(Melon):
-    pass
+    """Winter Squash (subclass of melon)"""
+
+    def paint(self):
+        """Paint the melon."""
+
+        super().prep()
+        robots.painterbot.paint(self)
